dca337bf04687569d36904bf36f9016bd482f051
Summer <sgbanister@gmail.com>
2020-02-29 17:17:56 -0800

    Edited Card_Deck.

---
+++ DeckOfCards/Assets/Scripts/DeckModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckModel : MonoBehaviour
+{
+    List<int> cards;
+
+    public IEnumerable<int> GetCards()
+    {
+        foreach (int i in cards)
+        {
+            yield return i;
+        }
+    }
+
+    public int Pop()
+    {
+        int temp = cards[0];
+        cards.RemoveAt(0);
+        return temp;
+    }
+
+    public void Push(int card)
+    {
+        cards.Add(card);
+    }
+
+    public void CreateDeck()
+    {
+        cards.Clear();
+
+        for (int i = 0; i < 52; i++)
+        {
+            cards.Add(i);
+        }
+
+        int n = cards.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            int temp = cards[k];
+            cards[k] = cards[n];
+            cards[n] = temp;
+        }
+    }
+
+    void Awake()
+    {
+        cards = new List<int>();
+        CreateDeck();
+    }
+}
